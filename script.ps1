939d34728933cab9e8017394cbd3cd611e689ad9
Alexander Arana Escobedo <117573697+aranaescobedo@users.noreply.github.com>
2023-01-02 22:49:21 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -55,10 +55,59 @@ az aks get-credentials -n $aksName -g $aksResourceGroup
 # Create namespace:
 kubectl create namespace hero
 
+echo @"
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    azure.workload.identity/client-id: $idClientId
+  labels:
+    azure.workload.identity/use: "true"
+  name: $serviceAccountName
+  namespace: $namespace
+"@ > serviceaccount.yml | kubectl apply -f serviceaccount.yml
+
 # Get the OIDC Issuer URL:
 $aks_oidc_issuer = "$(az aks show -n $aksName -g $aksResourceGroup --query "oidcIssuerProfile.issuerUrl" -otsv)"
 
 # Establish federated identity credential:
 az identity federated-credential create --name $ficid --identity-name $idName --resource-group $aksResourceGroup --issuer $aks_oidc_issuer --subject system:serviceaccount:${namespace}:${serviceAccountName}
+
+echo @"
+apiVersion: v1
+kind: Service
+metadata:
+  name: secret-service
+  namespace: $namespace
+spec:
+  type: LoadBalancer
+  ports:
+    - name: secret-port
+      protocol: TCP
+      port: 8080
+      targetPort: 8080
+  selector:
+    name: secret-pod
+"@ > service.yml | kubectl apply -f service.yml
+
+echo @"
+apiVersion: v1
+kind: Pod
+metadata:
+  name: secret-app
+  namespace: $namespace
+  labels:
+    name: secret-pod
+spec:
+  serviceAccountName: $serviceAccountName
+  containers:
+    - image: docker.io/aranaescobedo/workload-id-app-aks:1.0
+      name: secret-container
+      env:
+        - name: KEYVAULT_NAME
+          value: $kvName
+        - name: SECRET_NAME
+          value: $mySecretName
+"@ > pod.yml | kubectl apply -f pod.yml
 
 kubectl get service -n hero
